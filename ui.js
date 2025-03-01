15efaadd9794987aa3ef885298e89594cf64ea1d
Parth-Saini <sainiparth018@gmail.com>
2025-03-01 23:14:08 +0530

    updated

--- ui.js
+++ ui.js
@@ -58,3 +58,62 @@ function mediaManagement() {
     oldViewportWidth = newViewportWidth;
   }
 }
+
+const previous = document.querySelector("#previous");
+const nextSlide = document.querySelector("#nextSlide");
+
+let sliderViewport = document.querySelector("#sliderViewport");
+let sliderContent = document.querySelector("#sliderContent");
+let sliderWidth = sliderContent.getBoundingClientRect().width;
+
+let positionOffset = 0;
+
+const slideMovement =
+  parseFloat(
+    document.querySelector(".sliderItem").getBoundingClientRect().width,
+    10
+  ) +
+  parseFloat(
+    window.getComputedStyle(sliderContent).getPropertyValue("gap"),
+    10
+  );
+
+previous.addEventListener("click", () => {
+  if (positionOffset !== 0) {
+    positionOffset += slideMovement;
+    sliderContent.style.left = positionOffset + "px";
+  }
+});
+
+nextSlide.addEventListener("click", () => {
+  const viewportWidth = sliderViewport.getBoundingClientRect().width;
+  if (sliderWidth - Math.abs(positionOffset) > viewportWidth) {
+    positionOffset -= slideMovement;
+    sliderContent.style.left = positionOffset + "px";
+  }
+});
+
+const mediaQuerySmall = window.matchMedia("(max-width: 510px)");
+const mediaQueryMedium = window.matchMedia("(max-width: 770px)");
+
+mediaQuerySmall.addEventListener("change", updateMedia);
+mediaQueryMedium.addEventListener("change", updateMedia);
+
+let previousViewportWidth = window.innerWidth;
+
+function updateMedia() {
+  const newViewportWidth = window.innerWidth;
+
+  if (positionOffset <= -slideMovement && previousViewportWidth < newViewportWidth) {
+    positionOffset += slideMovement;
+    sliderContent.style.left = positionOffset + "px";
+    previousViewportWidth = newViewportWidth;
+  } else if (
+    positionOffset <= -slideMovement &&
+    previousViewportWidth > newViewportWidth
+  ) {
+    positionOffset -= slideMovement;
+    sliderContent.style.left = positionOffset + "px";
+    previousViewportWidth = newViewportWidth;
+  }
+}
